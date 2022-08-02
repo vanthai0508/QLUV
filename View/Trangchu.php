9d17c2e65902de9f99bafd75ebc2b3b450528b74
vanthai0508 <93816353+vanthai0508@users.noreply.github.com>
2022-08-02 20:47:18 +0700

    trangchu

---
+++ View/Trangchu.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="CSS/Trangchu.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="start">
+        <h1>MOR SOFTWARE JSC</h1>
+    </div>
+    <div class="column">
+        <div class="article">
+            <h1>Reduced development costs</h1>
+            <section>
+                By having Moa's SE stationed at your company, you can ensure quality and keep costs down to about half
+                of the cost in Japan.
+            </section>
+            <p>M O R</p>
+            <section>
+                <ul>
+                    <li>Đặng Lê Hùng</li>
+                    <li>Vũ Văn Tú</li>
+                    <li>Lê Mạnh Hùng</li>
+
+                </ul>
+            </section>
+            <section> <button>APPLY</button> </section>
+        </div>
+    </div>
+    <div class="column">
+        <div class="article">
+            <h1>human resources</h1>
+            <section>
+                Many of our employees graduated from prestigious universities in Vietnam, and are highly evaluated not
+                only for their technical capabilities, but also for their management and communication skills.
+            </section>
+            <p>M O R</p>
+            <section>
+                <ul>
+                    <li>Đặng Lê Hùng</li>
+                    <li>Vũ Văn Tú</li>
+                    <li>Lê Mạnh Hùng</li>
+                </ul>
+            </section>
+            <section> <button>APPLY</button> </section>
+        </div>
+    </div>
+    <div class="column">
+        <div class="article">
+            <h1>International environment</h1>
+            <section>
+                Many of our bridge SEs have experience studying or working in Japan, and are able to communicate with
+                Japanese customers in Japanese without discomfort.
+            </section>
+            <p>M O R</p>
+            <section>
+                <ul>
+                    <li>Đặng Lê Hùng</li>
+                    <li>Vũ Văn Tú</li>
+                    <li>Lê Mạnh Hùng</li>
+                </ul>
+            </section>
+            <section> <button>APPLY</button></section>
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
